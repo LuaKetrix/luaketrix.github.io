bcf7fb55c9ca28619bf65a31e35595b20581f220
plket <94329015+LuaKetrix@users.noreply.github.com>
2023-10-10 16:42:57 -0700

    Add files via upload

---
+++ Website4/function.js
@@ -0,0 +1,19 @@
+const spinning = document.getElementById("loading");
+const mainBody = document.getElementById("body")
+
+function changeWebsite() {
+    spinning.remove();
+    mainBody.style.backgroundColor = "rgb(0, 162, 255)";
+    mainBody.style.justifyContent = "start"
+    mainBody.style.alignItems = "start"
+    mainBody.style.display = "block"
+    mainBody.style.backgroundImage = "linear-gradient(to bottom right, rgba(0,0,0,255), rgba(19,15,63,255)"
+    mainBody.style.overflow = "scroll"
+         
+    const nodeList = document.querySelectorAll("p, h1, h2");
+    for (let i = 0; i < nodeList.length; i++) {
+    nodeList[i].style.opacity = 1;
+    }
+}
+
+setTimeout(() => changeWebsite.call(), 100);
